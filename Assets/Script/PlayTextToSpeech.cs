ec5b90bb620b576df9efe055ca327b1b17a98c32
Gerald <geraldchuah94@gmail.com>
2017-10-12 01:45:22 +0800

    v0.0.1[Add] Simple speech to text demo.

---
+++ Assets/Script/PlayTextToSpeech.cs
@@ -0,0 +1,90 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TextToSpeech;
+using System;
+
+[RequireComponent(typeof(AudioSource))]
+public class PlayTextToSpeech : MonoBehaviour
+{
+    [SerializeField] TextToSpeechManager manager;
+    [SerializeField] Button _button;
+    [SerializeField] InputField _inputField;
+    [SerializeField] Dropdown _languageDropDownList;
+    [SerializeField] Dropdown _audioFormatDropDownList;
+
+    private string text;
+    
+    AudioSource _audioSource;
+
+    private void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+
+        if (_button != null)
+        {
+            _button.onClick.AddListener(PlayVoice);
+        }
+
+        foreach (string name in Enum.GetNames((typeof(Language))))
+        {
+            _languageDropDownList.options.Add(new Dropdown.OptionData(name));
+        }
+        _languageDropDownList.value =3;
+
+        foreach (string name in Enum.GetNames((typeof(AudioCodecs))))
+        {
+            _audioFormatDropDownList.options.Add(new Dropdown.OptionData(name));
+        }
+        _audioFormatDropDownList.value = 3;
+    }
+
+    public void PlayVoice()
+    {
+        StartCoroutine("RunPlayVoice");
+    }
+
+    IEnumerator RunPlayVoice()
+    {
+        OnGetVoiceStart();
+
+        text = _inputField.text;
+        string url = manager.GetTextToSpeechAudioWithIndex(text, _languageDropDownList.value, _audioFormatDropDownList.value);
+        WWW www = new WWW(url);
+        yield return www;
+        AudioClip clip = www.GetAudioClip(false, false, manager.GetCurrentAudioType());
+        if (clip.length > 0)
+        {
+            _audioSource.clip = clip;
+            _audioSource.Play();
+        }      
+        else
+        {
+            Debug.LogError("Failed to get the voice. Please try:\n" +
+                "1.Try it in other languages.\n" +
+                "2.Fill in something in text field.\n" +
+                "3.Choose the correct audio format.");
+        }
+
+        OnGetVoiceEnd();
+    }
+
+    void OnGetVoiceStart()
+    {
+        if (_button != null)
+        {
+            _button.enabled = false;
+            _button.targetGraphic.color = _button.colors.disabledColor;
+        }
+    }
+
+    void OnGetVoiceEnd()
+    {
+        if (_button != null)
+        {
+            _button.enabled = true;
+            _button.targetGraphic.color = _button.colors.normalColor;
+        }
+    }
+}
---
+++ Assets/Script/TextToSpeechManager.cs
@@ -0,0 +1,176 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+
+namespace TextToSpeech
+{
+    public enum AudioCodecs
+    {
+        MP3=0,
+        WAV,
+        AAC,
+        OGG
+    }
+
+    public enum Language
+    {
+        Catalan=0,
+        Chinese_China,
+        Chinese_HongKong,
+        Chinese_Taiwan,
+        Danish,
+        Dutch,
+        English_Australia,
+        English_Canada,
+        English_GreatBritain,
+	    English_India,
+        English_UnitedStates,
+        Finnish,
+        French_Canada,
+        French_France,
+        German,
+        Italian,
+        Japanese,
+        Korean,
+        Norwegian,
+        Polish,
+        Portuguese_Brazil,
+        Portuguese_Portugal,
+        Russian,
+        Spanish_Mexico,
+        Spanish_Spain,
+        Swedish_Sweden
+    }
+
+    public class TextToSpeechOption
+    {
+        public static string[] language = new string[] 
+        {
+            "ca-es",
+            "zh-cn",   
+            "zh-hk",   
+            "zh-tw",   
+            "da-dk",   
+            "nl-nl",   
+            "en-au",   
+            "en-ca",   
+            "en-gb",   
+            "en-in",	
+            "en-us",   
+            "fi-fi",   
+            "fr-ca",   
+            "fr-fr",   
+            "de-de",   
+            "it-it",   
+            "ja-jp",   
+            "ko-kr",   
+            "nb-no",   
+            "pl-pl",   
+            "pt-br",   
+            "pt-pt",   
+            "ru-ru",   
+            "es-mx",   
+            "es-es",   
+            "sv-se"           
+        };
+
+        public static string[] audioCodecs = new string[]
+        {
+             "MP3",
+             "WAV",
+             "AAC",
+             "OGG"
+        };               
+    }        
+             
+    public class TextToSpeechManager : MonoBehaviour
+    {
+        public Language selectedLanguage = Language.English_UnitedStates;
+        public AudioCodecs selectedAudioCodecs = AudioCodecs.OGG;
+
+        private string _APIKey = "086fb34ec40c435ebb3b492c5742cdbb";
+
+        private string _language;
+        private string _source;
+        private string _audioFormat;
+        private string _url = "http://api.voicerss.org/?key={0}&hl={1}&src={2}&c={3}";
+
+        private string _currentLanguage
+        {
+            get
+            {
+                _language = TextToSpeechOption.language[(int)selectedLanguage];
+                return _language;
+            }
+        }
+
+        private string _currentAudioFormat
+        {
+            get
+            {
+                _audioFormat = TextToSpeechOption.audioCodecs[(int)selectedAudioCodecs];
+                return _audioFormat;
+            }
+        }
+
+        public AudioType GetAudioType(AudioCodecs audioCodecs)
+        {
+            AudioType type = AudioType.OGGVORBIS;
+            switch (audioCodecs)
+            {
+                case AudioCodecs.AAC:
+                    type=AudioType.ACC;
+                    break;
+
+                case AudioCodecs.MP3:
+                    type=AudioType.MPEG;
+                    break;
+
+                case AudioCodecs.OGG:
+                    type=AudioType.OGGVORBIS;
+                    break;
+
+                case AudioCodecs.WAV:
+                    type=AudioType.WAV;
+                    break;
+
+                default:
+                    type=AudioType.OGGVORBIS;
+                    break;
+            }
+
+            return type;
+        }
+
+        public AudioType GetCurrentAudioType()
+        {
+            return GetAudioType(selectedAudioCodecs);
+        }
+
+        public string GetTextToSpeechAudio(string content,Language language = Language.English_UnitedStates, AudioCodecs audioCodecs = AudioCodecs.OGG)
+        {
+            selectedLanguage = language;
+            selectedAudioCodecs = audioCodecs;
+
+            return SetupAudioURL(content, _currentLanguage, _currentAudioFormat);
+        }
+
+        //Language index Default = 10 (English-US) , Audio Format Index Default = 3(OGG)
+        public string GetTextToSpeechAudioWithIndex(string content, int languageIndex =10, int audioformatIndex =3)
+        {
+            string language = TextToSpeechOption.language[languageIndex];
+            string audioformat= TextToSpeechOption.audioCodecs[audioformatIndex];
+
+            return SetupAudioURL(content, language, audioformat);
+        }
+
+        private string SetupAudioURL(string content, string language, string audioFormat)
+        {  
+            Regex rgx = new Regex("\\s+");
+            _source = rgx.Replace(content, "%20");
+
+            return string.Format(_url, _APIKey, language, _source, audioFormat);
+        }
+    }
+}
